fe954f181691e4b59a557559e819453d6440ea9c
Yan <yangoryachev7@gmail.com>
2026-05-09 20:03:54 +0300

    add pydantic

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from typing import Annotated
-from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+from fastapi import FastAPI, Body, Path
 #pupupu yes yes yes
 app = FastAPI()
 
+class Model(BaseModel):
+    id : int = Field(gt=10)
+    username : str = Field(min_length=4)
+
 @app.get("/{id}")
-async def home(id:int | None):
-    return {"name":id}
+async def home(id:Annotated[int, Path(gt=3)]):
+    return id
+
+@app.post("/post")
+async def post(user:Annotated[Model, Body(max_length=10)]):
+    return user
