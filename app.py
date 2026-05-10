e7e324e0920d6c8ac4d74086809487618b83610c
Yan <yangoryachev7@gmail.com>
2026-05-10 14:17:19 +0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from typing import Annotated
 from pydantic import BaseModel, Field
 from fastapi import FastAPI, Body, Path
+from vnesh_app import vnesh_rout
+
 # teper seryozno
 app = FastAPI()
+app.include_router(vnesh_rout)
 
 class Model(BaseModel):
     id : int = Field(gt=10)
